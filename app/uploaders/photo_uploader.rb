96a02fd2a10e79ac08765bbe7faf6001e5b47d56
seanahrens <seanahrens@gmail.com>
2014-08-14 18:38:22 -0400

    no message

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -22,7 +22,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_fit => [200, 300]
+  process :resize_to_fill => [120, 120]
 
   #
   # def scale(width, height)
