a094f0c16e0ab2654a43be8db88d90d43c7847cf
seanahrens <seanahrens@gmail.com>
2014-08-15 00:39:19 -0400

    no message

--- app/controllers/pcori_controller.rb
+++ app/controllers/pcori_controller.rb
@@ -1,6 +1,16 @@
 class PcoriController < ApplicationController
   before_action :define_author
   before_action :catch_flash_message
+  before_action :set_body_class_to_idea, :only => [:idea_index,:idea_show,:idea_new]
+  before_action :set_body_class_to_study, :only => [:study_index,:study_show,:study_new]
+
+  def set_body_class_to_idea
+    @body_class = "idea"
+  end
+
+  def set_body_class_to_study
+    @body_class = "study"
+  end
 
   def define_author
     @author = User.first
