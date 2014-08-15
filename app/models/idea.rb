2d13ecf8ad24e7418e35ab0935bf6f9abfd19856
seanahrens <seanahrens@gmail.com>
2014-08-15 02:20:37 -0400

    basic role distinction

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,4 +2,10 @@ class Idea < ActiveRecord::Base
   belongs_to :author, :class => User
   acts_as_followable
 
+
+  def followers_count_by_role(role)
+    followers.count { |x| x.has_role?(role)}
+
+  end
+
 end
